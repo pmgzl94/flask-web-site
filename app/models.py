d675fef37e3f033bdd73f2c8fc7f983d869fdd03
gonzalo <gonzalo@localhost.localdomain>
2019-04-08 17:54:50 +0200

    [Add]: fonction to check login and registration

--- app/models.py
+++ app/models.py
@@ -1,17 +1,55 @@
+from flask import jsonify
 
 import config
 
-def add_database(title, begin, _end, _status)
-{
+def add_task(title, begin, _end, _status):
     sql = "INSERT INTO `users` (`title`, `begin`, `end`, `status`) VALUES (%s, %s, %s, %s)"
     cursor.execute(sql, (title, begin, _end, _status))
     config.connect.commit()
-}
 
-def remove_database(address)
-{
+def remove_task(address):
     mycursor = config.connect.cursor()
     sql = "DELETE FROM customers WHERE address = address"
     mycursor.execute(sql)
     config.connect.commit()
-}
+
+def check_log(email, password):
+    status = 0
+    cursor = config.connection.cursor()
+    cursor.execute("SELECT user from password")
+    datas = cursor.fetchall()
+    for data in datas:
+         if password == data:
+             status = 1
+    cursor.execute("SELECT user from email")
+    datas = curso.fetchall()
+    for data in datas:
+         if email == data and status == 1
+             return 0
+    return 1
+
+def check_log2(email, password):
+    status = 0
+    cursor = config.connection.cursor()
+    try:
+        affect_count = cursor.execute("SELECT " + email + ", " + password + " FROM " + user)
+        config.connection.commit()
+        logging.warn("%d", affected_count)
+    except MySQLdb.IntegrityError:
+        logging.warn("log failed")
+        status = 1
+    finally:
+        cursor.close()
+        return status
+
+def check_already_exist(name):
+    result = ""
+    try:
+         cursor = config.connection.cursor()
+         cursor.execute("SELECT name from user")
+         result = cursor.fetchall()
+         cursor.close()
+    except Exception as e :
+        print("Already exist")
+    print(result)
+    return 1
--- app/views.py
+++ app/views.py
@@ -6,30 +6,50 @@ from flask import render_template
 
 from flask import request
 
+
+
 import pymysql as sql
 
 import config
 
+import models
+
 @app.route('/', methods=['GET'])
-def route_home():
-    return render_template("index.html",
-                           title = "Home")
+def route_index():
+    if 'username' in session:
+        return render_template("home.html", title = "Home")
+    else:
+        return render_template("index.html", title = "Index")
 
 @app.route('/register', methods=['POST'])
 def register_page():
-    return render_template("register.html", title="REGISTER")
+    email = request.form["user_mail"]
+    password = request.form["user_password"]
+    password2 = request.form["user_password_confirm"]
+    if check_already_exist(name) == 1 and password == password2:
+        return render_template("register.html", title="REGISTER")
+    else:
+        Session['username'] = email
+        return render_template("home.html", title="HOME")
 
 @app.route('/signin', methods=['POST'])
 def log_page():
-    return render_template("log.html", title="LOG")
+    email = request.form["user_mail"]
+    password = request.form["user_password"]
+    if check_log2(email, password) == 1:
+        return render_template("log.html", title="LOG")
+    else:
+        Session['username'] = email
+        return render_template("home.html", title="HOME")
 
 @app.route('/signout', methods=['POST'])
 def register():
-    user_name = request.form["user_mail"]
-    print(user_name)
+    name = Session['username']
+    session.pop('username', None)
     return render_template("index.html",
-                    title="Hello ouarld",
-                    myContent="My SUPER content !!")
+                           title="Hello ouarld",
+                           myContent="My SUPER content !!")
+
 
 @app.route('/user/<username>', methods=['POST'])
 def  route_user(username):
